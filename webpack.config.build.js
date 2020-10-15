e03fe95571e7f51bdad7d734c36bb414f12c75a8
David Fox <david@newenglish.co.uk>
2020-10-15 15:53:20 +0100

    Fixed source map

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const chokidar = require('chokidar');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
 const THEME_NAME = 'newenglish-starter';
@@ -8,14 +8,26 @@ module.exports = {
     entry: { 'assets': './assets/assets.dev.js' },
     output: { path: path.resolve(__dirname, 'dist'), filename: '[name].js', publicPath: `/wp-content/themes/${THEME_NAME}/dist/`, },
     resolve: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx']
+        extensions: [ '.js', '.jsx', '.ts', '.tsx']
     },
-    mode: 'production',
+    mode: 'development',
     plugins: [
         new CleanWebpackPlugin({cleanAfterEveryBuildPatterns: ['!*.png', '!*.svg', '!*.jpg']}),
-        new MiniCssExtractPlugin({ filename: '[name].css', hunkFilename: "[id].css" }),
         new webpack.HotModuleReplacementPlugin()
     ],
+    devServer: {
+        hot: true,
+        compress: true,
+        contentBase: path.join(__dirname, 'dist'),
+        writeToDisk: true,
+        https: false,
+        disableHostCheck: true,
+        before(app, server) {
+            chokidar.watch([ './**/*.php'], {ignored: ['node_modules/**/*', '.git/**/*', '.cache/']}).on('all', function () {
+                server.sockWrite(server.sockets, 'content-changed');
+            })
+        },
+    },
     module: {
         rules: [
             {
@@ -34,9 +46,9 @@ module.exports = {
                 test: /\.s[ac]ss$/i,
                 exclude: /node_modules/,
                 use: [
-                    { loader: MiniCssExtractPlugin.loader, options: {hmr: true } },
-                    { loader: 'css-loader', options: { importLoaders: 1 }},
-                    'sass-loader',
+                    'style-loader',
+                    { loader: 'css-loader', options: { importLoaders: 1, sourceMap: true }},
+                    { loader: 'sass-loader', options: { sourceMap: true }},
                 ]
             },
             {
